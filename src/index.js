0487cf5dbefc2d7ec1cac979cabe71b9d31447e1
IbrahimMalgwi <ibrahimganamalgwi@outlook.com>
2023-11-22 09:12:49 +0100

    docker file and docker-compme

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const app = require('express')();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+app.get('/', (req, res) => 
+  res.json({message: 'Docker is easy '})
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => 
+  console.log(`App listening on http://localhost${port}`)
+);
